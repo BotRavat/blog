c75e0cb4434bc44289cfa3f507138f2e8649d268
RavaT <7sravat@gmail.com>
2023-07-17 01:10:42 +0530

    fetching single post and category added / project work resumed

--- api/index.js
+++ api/index.js
@@ -4,7 +4,11 @@ const app=express();
 const mongoose=require('mongoose');
 const authRoute=require('./routes/auth');
 const userRoute=require('./routes/users');
-const userPost=require('./routes/posts');
+const postRoute=require('./routes/posts');
+const categoryRoute=require('./routes/categories');
+const multer=require("multer");
+
+
 dotenv.config();
 app.use(express.json());
 
@@ -12,9 +16,13 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(console.log( "Connected to MongoDB")).catch((err)=>console.log(err));
 
 
+const storage=multer.diskStorage
+
+
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
-app.use("/api/posts",userPost);
+app.use("/api/posts",postRoute);
+app.use("/api/categories",categoryRoute);
 
 const PORT=process.env.PORT
 app.listen(PORT || 8800,()=>{
