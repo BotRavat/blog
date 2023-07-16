6da260c6272dd7f0875b0c5fc7dc90d04a837a28
RavaT <7sravat@gmail.com>
2023-07-17 01:11:06 +0530

    fetching single post and category added / project work resumed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 import Header from "../../component/header/Header";
 import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
+import axios from "axios";
 import "./home.css";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
+const [posts,setPosts]=useState([]);
+const location=useLocation();
+console.log(location)
+useEffect(()=>{
+   const fetchPosts=async ()=>{
+     const res=await axios.get("/posts");
+     setPosts(res.data);
+   }
+   fetchPosts();
+},[]);
+
   return (<>
       <Header />
     <div className="home">
-    <Posts/>
+    <Posts posts={posts}/>
     <Sidebar/>
     </div>
     </>
